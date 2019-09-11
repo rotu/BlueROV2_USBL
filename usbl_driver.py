9a0501c93b145f472db7c4694abd423925ab9ec8
Nick Nothom <nicknothom@gmail.com>
2019-09-11 16:20:28 -0500

    Working!

--- usbl_driver.py
+++ usbl_driver.py
@@ -15,76 +15,77 @@ from os import system
 
 # destination / output
 ip="192.168.2.2"
-portnum = 27000
+nmeaPort = 27000
+mavPort = 25100
 sockitOut = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# sockitOut.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-# sockitOut.setblocking(False)
+sockitOut.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sockitOut.setblocking(False)
 
 #Radius of Earth
 R = float(6378137)
-
-# gpsData = {
-#     'time_usec' : 0,                        # (uint64_t) Timestamp (micros since boot or Unix epoch)
-#     'gps_id' : 0,                           # (uint8_t) ID of the GPS for multiple GPS inputs
-#     'ignore_flags' : 56,                    # (uint16_t) Flags indicating which fields to ignore (see GPS_INPUT_IGNORE_FLAGS enum). All other fields must be provided.
-#     'time_week_ms' : 0,                     # (uint32_t) GPS time (milliseconds from start of GPS week)
-#     'time_week' : 0,                        # (uint16_t) GPS week number
-#     'fix_type' : 3,                         # (uint8_t) 0-1: no fix, 2: 2D fix, 3: 3D fix. 4: 3D with DGPS. 5: 3D with RTK
-#     'lat' : 0,                              # (int32_t) Latitude (WGS84), in degrees * 1E7
-#     'lon' : 0,                              # (int32_t) Longitude (WGS84), in degrees * 1E7
-#     'alt' : 0,                              # (float) Altitude (AMSL, not WGS84), in m (positive for up)
-#     'hdop' : 0,                             # (float) GPS HDOP horizontal dilution of position in m
-#     'vdop' : 0,                             # (float) GPS VDOP vertical dilution of position in m
-#     'vn' : 0,                               # (float) GPS velocity in m/s in NORTH direction in earth-fixed NED frame
-#     've' : 0,                               # (float) GPS velocity in m/s in EAST direction in earth-fixed NED frame
-#     'vd' : 0,                               # (float) GPS velocity in m/s in DOWN direction in earth-fixed NED frame
-#     'speed_accuracy' : 0,                   # (float) GPS speed accuracy in m/s
-#     'horiz_accuracy' : 0,                   # (float) GPS horizontal accuracy in m
-#     'vert_accuracy' : 0,                    # (float) GPS vertical accuracy in m
-#     'satellites_visible' : 0                # (uint8_t) Number of satellites visible.
-# }
-#
-# rthData = {
-#     'ab' : 0,
-#     'ac' : 0,
-#     'ae' : 0,
-#     'sr' : 0,
-#     'tb' : 0,
-#     'cb' : 0,
-#     'te' : 0,
-#     'er' : 0,
-#     'ep' : 0,
-#     'ey' : 0,
-#     'ch' : 0,
-#     'db' : 0
-# }
-#
-# rovData = {
-#     'time_usec' : 0,                        # (uint64_t) Timestamp (micros since boot or Unix epoch)
-#     'gps_id' : 0,                           # (uint8_t) ID of the GPS for multiple GPS inputs
-#     'ignore_flags' : 56,                    # (uint16_t) Flags indicating which fields to ignore (see GPS_INPUT_IGNORE_FLAGS enum). All other fields must be provided.
-#     'time_week_ms' : 0,                     # (uint32_t) GPS time (milliseconds from start of GPS week)
-#     'time_week' : 0,                        # (uint16_t) GPS week number
-#     'fix_type' : 3,                         # (uint8_t) 0-1: no fix, 2: 2D fix, 3: 3D fix. 4: 3D with DGPS. 5: 3D with RTK
-#     'lat' : 0,                              # (int32_t) Latitude (WGS84), in degrees * 1E7
-#     'lon' : 0,                              # (int32_t) Longitude (WGS84), in degrees * 1E7
-#     'alt' : 0,                              # (float) Altitude (AMSL, not WGS84), in m (positive for up)
-#     'hdop' : 0,                             # (float) GPS HDOP horizontal dilution of position in m
-#     'vdop' : 0,                             # (float) GPS VDOP vertical dilution of position in m
-#     'vn' : 0,                               # (float) GPS velocity in m/s in NORTH direction in earth-fixed NED frame
-#     've' : 0,                               # (float) GPS velocity in m/s in EAST direction in earth-fixed NED frame
-#     'vd' : 0,                               # (float) GPS velocity in m/s in DOWN direction in earth-fixed NED frame
-#     'speed_accuracy' : 0,                   # (float) GPS speed accuracy in m/s
-#     'horiz_accuracy' : 0,                   # (float) GPS horizontal accuracy in m
-#     'vert_accuracy' : 0,                    # (float) GPS vertical accuracy in m
-#     'satellites_visible' : 0                # (uint8_t) Number of satellites visible.
-# }
 
 sampleRTH = "$USRTH,358.5,1.5,2.8,142.8,52.8,37.2,2.8,-0.6,1.9,178.1,271.9,16*49"
 sampleGPS = "$GPGGA,200838.400,3845.0630,N,07727.2770,W,1,06,1.2,111.4,M,-33.4,M,,0000*6A"
 # rthData = pynmea2.parse(sampleRTH)
 # gpsData = pynmea2.parse(sampleGPS)
 # rovData = pynmea2.parse(sampleGPS)
+
+def sendToMav(newLat, newLon):
+    result = {}
+    result['time_usec'] = str(gpsData.timestamp)           #Timestamp (micros since boot or Unix epoch)
+    result['gps_id'] = 0                #ID of the GPS for multiple GPS inputs
+    result['ignore_flags'] = 1|2|4|8|16|32|64|128    #Flags indicating which fields to ignore (see GPS_INPUT_IGNORE_FLAGS enum). All other fields must be provided.
+    result['time_week_ms'] = 0          #GPS time (milliseconds from start of GPS week)
+    result['time_week'] = 0             #GPS week number
+    result['fix_type'] = 3              #0-1: no fix, 2: 2D fix, 3: 3D fix. 4: 3D with DGPS. 5: 3D with RTK
+    result['lat'] = 20*1e7                   #Latitude (WGS84), in degrees * 1E7
+    result['lon'] = 30                   #Longitude (WGS84), in degrees * 1E7
+    result['alt'] = 0                   #Altitude (AMSL, not WGS84), in m (positive for up)
+    result['hdop'] = 1                  #GPS HDOP horizontal dilution of position in m
+    result['vdop'] = 1                  #GPS VDOP vertical dilution of position in m
+    result['vn'] = 0                    #GPS velocity in m/s in NORTH direction in earth-fixed NED frame
+    result['ve'] = 0                    #GPS velocity in m/s in EAST direction in earth-fixed NED frame
+    result['vd'] = 0                    #GPS velocity in m/s in DOWN direction in earth-fixed NED frame
+    result['speed_accuracy'] = 0        #GPS speed accuracy in m/s
+    result['horiz_accuracy'] = 0        #GPS horizontal accuracy in m
+    result['vert_accuracy'] = 0         #GPS vertical accuracy in m
+    result['satellites_visible'] = 0    #Number of satellites visible.
+    result = json.dumps(result)
+    print(result)
+    sockitOut.sendto(result.encode(), (ip, mavPort))
+
+def sendtoNMEARX(newLat, newLon):
+    gts = str(gpsData.timestamp)
+    timestamp = float(gts[0:2] + gts[3:5] + gts[6:11])
+    gds = str(gpsData.datestamp)
+    datestamp = str(gds[8:10] + gds[5:7] + gds[2:4])
+    #pdb.set_trace()
+    newMessage = pynmea2.RMC('GN', 'RMC',
+        (
+        str(timestamp),
+        str(gpsData.status),
+        str(newLat*100)[0:10],
+        str(gpsData.lat_dir),
+        str(newLon*100)[0:10],
+        str(gpsData.lon_dir),
+        str(gpsData.spd_over_grnd),
+        str(""),
+        str(datestamp),
+        str(gpsData.mag_variation),
+        str(gpsData.mag_var_dir)
+        )
+    )
+    print("G: " , str(gpsData))
+    print("N: " , str(newMessage))
+    #sockitOut.sendto(b'$GNGGA,191732.20,4458.18069,N,09331.05618,W,2,12,0.80,310.1,M,-30.7,M,,0000*76', (ip, nmeaPort))
+
+    sockitOut.sendto(str(newMessage).encode(), (ip, nmeaPort))
+
+
+    #sockitOut.sendto(str(gpsData), (ip, nmeaPort))
+
+#gpsData = pynmea2.parse("$GNGGA,191731.40,4458.18062,N,09331.05604,W,2,12,0.80,309.9,M,-30.7,M,,0000*75")
+#rthData = pynmea2.parse("$USRTH,20.0,-20.0,30.0,14.8,49.3,40.7,28.3,-0.3,1.7,29.5,60.5,72*51");
 
 while True:
     #Update Data
@@ -93,21 +94,17 @@ while True:
             streamreader = pynmea2.NMEAStreamReader(dev)
             for msg in streamreader.next():
                 rthData = msg
-    except:
-        print("Failed to read RTH")
 
-
-    try:
         with open('/dev/ttyACM0', 'r') as dev:
             streamreader = pynmea2.NMEAStreamReader(dev)
             for msg in streamreader.next():
                 gpsData = msg
-                rovData = msg
     except:
-        print("Failed to read RTH")
+        print("Failed to read input")
 
     #Maths
     #Elevation 0 at horizon?
+    #pdb.set_trace()
     hr = rthData.sr * math.cos(rthData.te)
 
     dn = math.cos(rthData.cb) * hr
@@ -118,14 +115,8 @@ while True:
 
     dLon = de / (R * math.cos(math.pi) * gpsData.latitude / float(100))
 
-    newLat = gpsData.latitude + dLat * 180 / math.pi
-    newLon = gpsData.longitude + dLon * 180 / math.pi
+    newLat = abs(gpsData.latitude + dLat * 180 / math.pi)
+    newLon = abs(gpsData.longitude + dLon * 180 / math.pi)
 
-    timestring = (str(gpsData.timestamp)[0:2] + str(gpsData.timestamp)[3:5] + str(gpsData.timestamp)[6:8])
-    rovSentence = pynmea2.GGA('GP', 'GGA', (timestring, str(newLat*100), str(gpsData.lat_dir), str(newLon*100), str(gpsData.lon_dir)))
-    print(str(gpsData))
-    #pdb.set_trace()
-    #Send ROV data
-    # print("G: " , gpsData)
-    # print("R: " , rovData)
-    sockitOut.sendto(str(rovSentence), (ip, portnum))
+    #sendToMav(newLat, newLon)
+    sendtoNMEARX(newLat, newLon)
