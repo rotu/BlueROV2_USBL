273e9a183e3764f1b4ea96e619a1e5bc656adf55
Nick Nothom <nicknothom@gmail.com>
2019-10-03 15:37:53 -0500

    Comments

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+#Configure the USBL Serial Port
 stty -F /dev/ttyUSB0 115200 -brkint -icrnl -imaxbel -opost -isig -icanon -iexten -echo -echoe -echok -echoctl -echoke
 
+#Send the GPS data to QGC over UDP
 screen -dm -S gpsToQGC socat -u FILE:/dev/ttyACM0,b4800,raw,ignoreeof UDP4-DATAGRAM:192.168.2.1:14401,reuseaddr,ignoreeof
